afa2ee903e499882d89615f15079036ff9acab2a
17sushmita <17sushmita@gmail.com>
2021-01-23 17:29:49 +0530

    Initial Commit

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] \
-        = 'postgres://postgres:sushmita@localhost:5432/kanban'
+        = os.environ['DATABASE_URL']
 
     from app.database.sqlalchemy_extension import db
 
